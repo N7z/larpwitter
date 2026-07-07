cdf366f50f0482ab2bb42b48db9d38b33a38ae48
Paulo Junior <paulonp@proton.me>
2026-07-07 18:57:15 -0300

    feat: add rate-limit to posts

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Validation\ValidationException;
 
 class StorePostRequest extends FormRequest
 {
@@ -20,5 +22,39 @@ class StorePostRequest extends FormRequest
             'body' => ['required', 'string', 'max:280'],
             'image' => ['nullable', 'image', 'mimes:jpeg,png,webp,gif', 'max:5120'],
         ];
+    }
+
+    /**
+     * Handle a passed validation attempt.
+     */
+    protected function passedValidation(): void
+    {
+        $this->ensureIsNotRateLimited();
+
+        RateLimiter::hit($this->throttleKey(), 60);
+    }
+
+    /**
+     * Ensure the post request is not rate limited.
+     */
+    protected function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'body' => "You're posting too fast. Please wait {$seconds} seconds and try again.",
+        ]);
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     */
+    protected function throttleKey(): string
+    {
+        return 'post:'.$this->user()->id;
     }
 }
